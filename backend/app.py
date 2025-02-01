b129991dbb3a835e4aa56ed3682f8f8313c7c6c4
Hector Luciano <hlucianojr@gmail.com>
2025-02-01 11:44:53 -0500

    making progress with my prediction app

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
-
+from HTMLGenerator import HTMLGenerator
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET'])
 def predict():
     # Future implementation for prediction logic
-    return jsonify({'message': 'Prediction logic not implemented yet.'})
+    generator = HTMLGenerator()
+    return generator.generate_html("https://statsapi.mlb.com/api/v1/sports");
+    # r eturn jsonify({'message': 'Prediction logic not implemented yet.'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
